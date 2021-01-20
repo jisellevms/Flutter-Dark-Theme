aea5c9e49a7dfbbefacda49646d0e869367e852e
Jiselle Martins <jisellevms@gmail.com>
2021-01-20 09:45:07 -0300

    subindo projeto

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project/app_controller.dart';
 
 import 'home_page.dart';
 
-class MyWidget extends StatelessWidget{
-
-
-
+class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.red),
-      home: HomePage(),
+    return AnimatedBuilder(
+      animation: AppController.instance,
+      builder: (BuildContext context, Widget child) {
+        return MaterialApp(
+          theme: ThemeData(
+              primarySwatch: Colors.red,
+              brightness: AppController.instance.isDartTheme
+                  ? Brightness.dark
+                  : Brightness.light),
+          home: HomePage(),
+        );
+      },
     );
   }
 }
-
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project/app_controller.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,14 +8,17 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int counter = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Contador")),
       body: Center(
-          child: GestureDetector(
-        child: Text("Contador: $counter"),
-        onTap: () {},
+          child: Switch(
+        value: AppController.instance.isDartTheme,
+        onChanged: (value) {
+           AppController.instance.changeTheme();
+        },
       )),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
